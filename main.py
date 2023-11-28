da7198905ef35df45ba83e0ca768c6db7f046e7e
leviives28 <149870357+leviives28@users.noreply.github.com>
2023-11-28 09:56:26 +0000

    Update main.py for additional protein grouping
    Added in two new inputs and a function call to facilitate additional grouping

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import files
 import output
 import optimize
 import validate
+import protein_sorting
 
 # Call input functions and save their return values into relevant variables
 input_file = inputs.file_input()
@@ -12,6 +13,8 @@ ordering = inputs.ordering_input()
 group_size = inputs.group_size_input()
 minimum_difference = inputs.minimum_difference_input()  
 exact_mass_column = inputs.exact_mass_column_input()
+protein_mass = inputs.protein_mass_input()
+fragment_id_column = inputs.fragment_id_column_input()
 
 # Call the read and sort file function and save its return value to the file variable
 file = files.read_and_sort_file(input_file, exact_mass_column)
@@ -33,8 +36,11 @@ else:
     # Call the sort outputs function and save the return value to the well_outputs variable
     well_outputs = output.sort_outputs(result_groups, receiver_plate_location, receiver_plate_format, ordering)
 
+    # Sort the protein total for each group item
+    sorted_protein_values = protein_sorting.sort_protein_totals(well_outputs, protein_mass, exact_mass_column, group_size, fragment_id_column)
+    
     # Add the output columns to the dict key column_names
     file["column_names"] += ["Output Well Plate", "Output Row", "Output Column"]
 
     # Call the save output function
-    files.save_output(file["column_names"], well_outputs)
+    files.save_output(file["column_names"], well_outputs, sorted_protein_values)
